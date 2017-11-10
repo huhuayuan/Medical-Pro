1f11e466ab4cc3ea76cb4dc4796893a9109af279
YangBo <1043951234@qq.com>
2017-11-10 19:27:16 +0800

    '2017-11-10/19-30'

--- pages/DmyProfit/DmyProfit.js
+++ pages/DmyProfit/DmyProfit.js
@@ -1,6 +1,12 @@
 // pages/DmyProfit/DmyProfit.js
 Page({
-  data:{},
+  data:{
+    doctor:{
+      image:'../../images/doctor.png',
+      name:'张某某',
+      profit:100.00
+    }
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
