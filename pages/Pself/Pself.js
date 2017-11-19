55672fbc00504bbf02802e67463a892efe749383
yuan <584514950@qq.com>
2017-11-19 11:58:33 +0800

    患者页面

---
+++ pages/Pself/Pself.js
@@ -0,0 +1,53 @@
+//doctorList.js 医生列表页面
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    userInfo: {},
+    myInfo:{
+      name:'张某某',
+      image: '../../images/doctor.png',
+      sex: '男',
+      tel: 18812345678,
+      age: 36,
+      myEarnings: 200,
+      doctorEarnings: 200,
+      billTotal: 10,
+      bills: [
+        {
+          num: 123456,
+          name:'感冒颗粒'
+        },{
+          num: 123456,
+          name:'感冒颗粒'
+        },{
+          num: 123456,
+          name:'感冒颗粒'
+        }
+      ]
+    }
+},
+  onLoad: function(options) {
+    let that = this;
+    that.data.doctorId = options.doctorId;
+    getApp().getDoctorInfo({doctorId: options.doctorId}, function(data) {
+      that.setData({
+          doctorInfo: data
+      });
+      console.log(data)
+    });
+
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+
+  },
+  openMap: function () {
+    wx.navigateTo({
+      url: '../Gmap/Gmap'
+    })
+  }
+})
